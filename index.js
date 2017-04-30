91cf503c5dd9fa1801004bc9497b704b117455cd
Siew Peng <Siew Peng>
2017-05-01 02:02:32 +0800

    Signed-off-by: Siew Peng <Siew Peng>

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ function processPostback(event) {
         name = bodyObj.first_name;
         greeting = "Hi " + name + ". ";
       }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      var message = greeting + "My name is Support Bot. I can tell you various details regarding your problem. What would you like to know about?";
       sendMessage(senderId, {text: message});
     });
   }
